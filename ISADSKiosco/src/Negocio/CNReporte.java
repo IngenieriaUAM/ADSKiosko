a8548f75c21f5483cfec2b1af53ab78577e40ca8
Cecy <Cecy@Cecy-PC>
2015-03-30 16:39:26 -0600

    Documentacion y limpieza en CNReporte

--- ISADSKiosco/src/Negocio/CNReporte.java
+++ ISADSKiosco/src/Negocio/CNReporte.java
@@ -18,24 +18,46 @@ public class CNReporte {
     
     private CDReporte oDReporte;
 
+    /**
+     *
+     * @param oDReporte
+     */
     public CNReporte(CDReporte oDReporte) {
         this.oDReporte = oDReporte;
     }
 
+    /**
+     * Constructor de la clase
+     */
     public CNReporte() {
        oDReporte=new CDReporte();
     }
     
+    /**
+     * Negocia una lista con un objeto de datos
+     * @return ArrayList con los requisitos de la base de datos
+     * @throws SQLException en caso de error SQL
+     */
     public ArrayList<CVOReporte> getListaClientes() throws SQLException {
         return oDReporte.getListaReporte();
     }
     
+    /**
+     * Agrega el reporte a CVOReporte
+     * @param oVReporte
+     * @throws SQLException en caso de error SQL
+     */
     public void setAgregaReporte(CVOReporte oVReporte) throws SQLException{
         System.out.println(oVReporte.getIdCliente());
         oDReporte.setAgregaReporte(oVReporte);
         
     }
     
+    /**
+     * Obtiene el reporte
+     * @return Lista de clientes
+     * @throws SQLException en caso de error SQL
+     */
     public int getIdReporte() throws SQLException{
         try{
         return this.getListaClientes().size()+1;
