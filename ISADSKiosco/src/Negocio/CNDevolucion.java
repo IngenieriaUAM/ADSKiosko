8998b8c95850b9e74eecd7c9231ac90af09d9aec
Cecy <Cecy@Cecy-PC>
2015-03-30 16:38:42 -0600

    Documentacion y limpieza en CNDevolucion

--- ISADSKiosco/src/Negocio/CNDevolucion.java
+++ ISADSKiosco/src/Negocio/CNDevolucion.java
@@ -10,22 +10,27 @@ import VO.CVODevolucion;
 import Datos.CDDevolucion;
 
 /**
- *
  * @author lucas
  */
+
 public class CNDevolucion {
     
     private CDDevolucion oDDevolucion;
 
+    /**
+     * Constructor de la clase
+     */
     public CNDevolucion() {
         oDDevolucion = new CDDevolucion();
     }
     
+    /**
+     * Agrega una devolucion en CVODevolucion
+     * @param poVDevolucion
+     * @throws SQLException en caso de error SQL
+     */
     public void setAgregaDevolucion(CVODevolucion poVDevolucion) throws SQLException{
         oDDevolucion.setAgregaDevolucion(poVDevolucion);
     }
-
-
-
-
+    
 }
