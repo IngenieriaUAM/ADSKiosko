491475dcebd6611f281c036fe04063f49fae0723
ehNetito <neto@winry>
2015-03-31 17:15:10 -0600

    Limpieza de clases, ojala suba esta porqueria ya me arte de que nomas no se vean reflejados mis cambios

--- ISADSKiosco/src/VO/CVOReporte.java
+++ ISADSKiosco/src/VO/CVOReporte.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package VO;
 
 /**
@@ -105,7 +100,5 @@ public class CVOReporte {
     
     public void setIdRenta(Long idRenta){
         this.idRenta=idRenta;
-    }
-    
-    
+    }   
 }
